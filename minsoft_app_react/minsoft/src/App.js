900b31b5d267a9d741a6287278900ca85375a034
DavidG <dago92@hotmail.com>
2021-10-08 21:34:53 -0500

    Adición de Bootstrap y navbar

--- minsoft_app_react/minsoft/src/App.js
+++ minsoft_app_react/minsoft/src/App.js
@@ -1,12 +1,17 @@
-import logo from './logo.png';
+import React, {Component} from "react";
+import Navigation from './components/navigation.jsx';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    <div className="App"> 
+      <Navigation/>
+      {/* <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      {/* </header> */} 
     </div>
   );
 }
---
+++ minsoft_app_react/minsoft/src/components/navigation.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import './Navigation.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import logo from './logo.png';
+
+
+
+function navigation() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark ">
+            <div className="container-fluid">
+                <img src={logo} className="App-logo" alt="logo" />
+                <a className="navbar-brand" href="/">Minsoft Coffee</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Productos</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Ventas</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">Usuarios</a>
+                        </li>
+                    </ul>
+                    <form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" />
+                        <button className="btn btn-outline-secondary" type="submit">Buscar</button>
+                    </form>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+
+export default navigation;
