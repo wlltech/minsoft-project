5381dedd98b4361289aeaeb02d8757b24b80e396
DavidG <dago92@hotmail.com>
2021-10-10 20:18:16 -0500

    Actualización estilos Nav-Bar

--- minsoft_app_react/minsoft/src/components/navigation.jsx
+++ minsoft_app_react/minsoft/src/components/navigation.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import './Navigation.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FaUserAlt } from "react-icons/fa";
 import logo from './logo.png';
 
 
-
 function navigation() {
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark ">
             <div className="container-fluid">
                 <img src={logo} className="App-logo" alt="logo" />
-                <a className="navbar-brand" href="/">Minsoft Coffee</a>
+                <a className="navbar-brand" href="/">Café Minsoft</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="/">Home</a>
+                            <a className="nav-link active" aria-current="page" href="/">Inicio</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="/">Productos</a>
@@ -27,12 +27,13 @@ function navigation() {
                             <a className="nav-link" href="/">Ventas</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="/">Usuarios</a>
+                            <a className="nav-link" href="/">Usuarios <i class="fa fa-user" aria-hidden="true"><FaUserAlt /></i> </a>
                         </li>
                     </ul>
                     <form className="d-flex">
                         <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Search" />
-                        <button className="btn btn-outline-secondary" type="submit">Buscar</button>
+                        <button className="btn btn-outline-secondary" type="submit">
+                            Buscar</button>
                     </form>
                 </div>
             </div>
---
+++ minsoft_app_react/minsoft/src/components/sells.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import './Sells.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import { FaUserAlt } from "react-icons/fa";
+import logo from './logo.png';
