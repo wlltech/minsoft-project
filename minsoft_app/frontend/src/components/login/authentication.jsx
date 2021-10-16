f543d18fc204d9b01b92ad99b902d747cafb8706
DavidG <dago92@hotmail.com>
2021-10-16 11:06:12 -0500

    Archivo creado en clase

--- minsoft_app/frontend/src/components/login/authentication.jsx
+++ minsoft_app/frontend/src/components/login/authentication.jsx
@@ -1,19 +1,39 @@
 import GoogleLogin from "react-google-login"
-import { Button } from 'reactstrap';
+// import { Button } from 'reactstrap';
+import React, { axios } from 'react';
+
 
 
 export default function Auth() {
 
 
+    
+        const respGoogle = async (resp) => {
+
+            const newUser = {
+                name: resp.profileObj.name,
+                email: resp.profileObj.email
+            }
+            const datosUser = await axios.post("http://localhost:8080/api/users", newUser);
+            
+            console.log(datosUser)
+
+        }
+
+
 
     return(
-        <Button 
-        className="btn col-sm-4"
+        <div className="btn">
+        <GoogleLogin 
         clientId=""
         buttonText="Login"
-        onSuccess="{}"
-        onFailure="{}"
+        onSuccess={respGoogle}
+        onFailure={respGoogle}
+        
+        cookiePolicy={"single_host_origin"}
+        >Login</GoogleLogin>
 
-        ></Button>
+  
+        </div>
     )
 }
