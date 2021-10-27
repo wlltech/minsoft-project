126eb37bd671111d7c8e0b654a55998d153c5603
Jovar879 <jovar879@hotmail.com>
2021-10-26 20:50:28 -0500

    Login con google

---
+++ minsoft_app/frontend/src/google-login.js
@@ -0,0 +1,20 @@
+export const loadGoogleScript = () => {
+  
+    //loads the Google JavaScript Library
+    (function () {
+        const id = 'google-js';
+        const src = 'https://apis.google.com/js/platform.js';
+        
+        //we have at least one script (React)
+        const firstJs = document.getElementsByTagName('script')[0];
+        
+        //prevent script from loading twice
+        if (document.getElementById(id)) { return; }
+        const js = document.createElement('script'); 
+        js.id = id;
+        js.src = src;
+        js.onload = window.onGoogleScriptLoad; 
+        firstJs.parentNode.insertBefore(js, firstJs);
+    }());    
+    
+}
